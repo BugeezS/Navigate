4ab58bb1b05aee29607133555b1bd971bc6f8ee4
Bruno <brunolb.lafont@gmail.com>
2023-04-17 10:16:45 +0200

    add ex 2

--- asset/script.js
+++ asset/script.js
@@ -5,3 +5,12 @@ const olLastChild = ol.children [4];
 
 ol.insertBefore(olLastChild, olFirstChild);
 
+
+const titre= document.querySelectorAll("h2")[2];
+const section = document.querySelectorAll("section");
+const titre2 = section[1].children[0];
+console.log(titre2);
+
+section[2].appendChild(titre2);
+
+
